db0d8563d8046c07ad9eb6334f9de5625fa62e5b
Natalie Menato <15682036+nmeusling@users.noreply.github.com>
2023-12-21 17:40:23 -0500

    Finish astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://app.netlify.com/sites/sparkly-rabanadas-ef3541/deploys/651cccb1ff95b70008080126",
+  integrations: [preact()]
+});
--- src/pages/posts/post-1.md
+++ src/pages/posts/post-1.md
@@ -4,6 +4,7 @@ title: "My First Blog Post"
 pubDate: 2023-10-03
 description: "This is my first post!"
 author: "Natalie Menato"
+tags: ["astro", "blogging", "test"]
 ---
 # Welcome
 Hello I am learning about Astro! This is my first markdown section
